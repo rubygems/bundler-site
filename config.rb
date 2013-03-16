454e3409dc5dbc9cb7553cd38cbbf407bb546bea
Nick Slocum <nslocum@gmail.com>
2013-03-16 15:12:23 -0400

    Added haml filters.

--- config.rb
+++ config.rb
@@ -55,6 +55,9 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
+  # Include all files in /lib
+  Dir[File.dirname(__FILE__) + '/lib/**/*.rb'].each {|file| require file }
+
   # For example, change the Compass output style for deployment
   # activate :minify_css
 

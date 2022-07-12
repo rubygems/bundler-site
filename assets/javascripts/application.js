d6268af24d5ee58b8480cc7a25abd1117d69e80b
Takuya Noguchi <takninnovationresearch@gmail.com>
2022-07-12 07:19:34 +0000

    Bump Bootstrap from 4.6.1 to 5.1.3
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,8 +1,3 @@
-import 'bootstrap/js/dist/button'
-import 'bootstrap/js/dist/tooltip'
-import 'bootstrap/js/dist/popover'
-import 'bootstrap/js/dist/dropdown'
-import 'bootstrap/js/dist/collapse'
 import './search'
 import './search_arrows'
 import AnchorJS from 'anchor-js';

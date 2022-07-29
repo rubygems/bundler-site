599818aae5625492654fbdcab1f3408b89fb65fa
Takuya Noguchi <takninnovationresearch@gmail.com>
2022-07-29 16:00:29 +0000

    Clean up application.js around AnchorJS
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,9 +1,2 @@
 import './search';
 import './search_arrows';
-import AnchorJS from 'anchor-js';
-
-const anchors = new AnchorJS();
-
-anchors.options = {
-  visible: 'touch',
-};
--- assets/javascripts/two_column_layout.js
+++ assets/javascripts/two_column_layout.js
@@ -2,13 +2,17 @@ import AnchorJS from 'anchor-js';
 
 const anchors = new AnchorJS();
 
-const el = document.querySelector('.version-selects');
-el &&
-  el.addEventListener('input', function (e) {
-    document.location.href = e.target.value;
-  });
+anchors.options = {
+  visible: 'touch',
+};
 
 anchors.add(
   '#page-content-wrapper h1, #page-content-wrapper h2, #page-content-wrapper h3, ' +
     '#page-content-wrapper h4, #page-content-wrapper h5'
 );
+
+const el = document.querySelector('.version-selects');
+el &&
+  el.addEventListener('input', function (e) {
+    document.location.href = e.target.value;
+  });

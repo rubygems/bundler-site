86722a193c28a9e355c56d7003b580be2e87430b
Takuya Noguchi <takninnovationresearch@gmail.com>
2023-01-01 00:50:48 +0000

    Use the numbered parameters in calculating VERSIONS
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>

--- lib/versions.rb
+++ lib/versions.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-VERSIONS = Dir.chdir("source") { Dir.glob("v*").sort_by{|x| Gem::Version.new(x[1..-1]) } }.freeze
+VERSIONS = Dir.chdir("source") { Dir.glob("v*").sort_by{ Gem::Version.new(_1[1..-1]) } }.freeze

de6db8c3c229aef16757439546380b18adaa531c
Takuya Noguchi <takninnovationresearch@gmail.com>
2023-01-02 07:18:27 +0000

    Unify groups guide into version-independent one
    Signed-off-by: Takuya Noguchi <takninnovationresearch@gmail.com>

--- config.rb
+++ config.rb
@@ -110,12 +110,12 @@ end
   # Add the rule above here if you need it for v1.15 as well
   next if version == "1.15"
 
-  %w[deploying faq git groups updating_gems].each do |filename|
+  %w[deploying faq git updating_gems].each do |filename|
     redirect "v#{version}/#{filename}.html", to: "v1.15/guides/#{filename}.html"
   end
 
   # Redirect versioned-guides (which are not localizable) on v1.12-v1.14 to version-independent guides
-  %w[bundler_setup bundler_sharing git_bisect rails sinatra].each do |filename|
+  %w[bundler_setup bundler_sharing git_bisect groups rails sinatra].each do |filename|
     redirect "v#{version}/#{filename}.html", to: "guides/#{filename}.html"
   end
 
@@ -130,7 +130,7 @@ end
 end
 
 # Redirect versioned-guides (which are not localizable) on v1.15 and below to version-independent guides
-%w[bundler_setup bundler_sharing git_bisect rails sinatra].each do |filename|
+%w[bundler_setup bundler_sharing git_bisect groups rails sinatra].each do |filename|
   redirect "v1.15/guides/#{filename}.html", to: "guides/#{filename}.html"
 end
 
